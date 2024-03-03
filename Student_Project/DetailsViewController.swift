48be6033c59cb681f85ceeb2dff55ddc71da89eb
aebinessa <abinessa0004@sdsu.edu>
2024-03-03 13:56:57 +0300

    first commit

---
+++ Student_Project/DetailsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsViewController.swift
+//  Student_Project
+//
+//  Created by Abdullah Bin Essa on 3/3/24.
+//
+
+import Foundation
---
+++ Student_Project/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  Student_Project
+//
+//  Created by Abdullah Bin Essa on 3/3/24.
+//
+
+import Foundation
